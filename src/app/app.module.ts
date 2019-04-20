c6717cc5952609d0cafd7b990fa3324975b05e5f
ochiengdoa <ochiengdavid44@gmail.com>
2019-04-20 23:03:07 +0300

    library first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+import { BookEditComponent } from './book-edit/book-edit.component';
+import { BookCreatesComponent } from './book-creates/book-creates.component';
+import { AppRouterModuleModule } from './app-router-module.module';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { BookService } from './book.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BookDetailComponent } from './book-detail/book-detail.component';
 
+// let's set a few routing
+// HashLocationStrategy and LocationStrategy imports prevents 404
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    BookDetailComponent,
+    BookCreatesComponent,
+    BookEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, HttpClientModule, AppRouterModuleModule
+
   ],
-  providers: [],
+  providers: [BookService, {provide: LocationStrategy, useClass: HashLocationStrategy} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
